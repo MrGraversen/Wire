d6429278c40317d5656827b72d744f979379d57e
Martin Graversen <martin@graversen.io>
2019-08-16 18:48:53 +0200

    :sparkles: Hetzner Cloud: Create server with SSH keys, labels, cloud init

--- src/main/java/io/graversen/wire/hcloud/CreateServer.java
+++ src/main/java/io/graversen/wire/hcloud/CreateServer.java
@@ -2,15 +2,28 @@ package io.graversen.wire.hcloud;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.util.List;
+import java.util.Map;
+
 public class CreateServer {
     private final String name;
     @SerializedName("server_type")
     private final String serverType;
     private final String image;
+    private final String location;
+    @SerializedName("user_data")
+    private final String userData;
+    private final Map<String, String> labels;
+    @SerializedName("ssh_keys")
+    private final List<String> sshKeys;
 
-    public CreateServer(String name, String serverType, String image) {
+    public CreateServer(String name, String serverType, String image, String location, String userData, Map<String, String> labels, List<String> sshKeys) {
         this.name = name;
         this.serverType = serverType;
         this.image = image;
+        this.location = location;
+        this.userData = userData;
+        this.labels = labels;
+        this.sshKeys = sshKeys;
     }
 }
--- src/main/java/io/graversen/wire/hcloud/Server.java
+++ src/main/java/io/graversen/wire/hcloud/Server.java
@@ -2,19 +2,23 @@ package io.graversen.wire.hcloud;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.util.Map;
+
 public class Server {
     private final int id;
     private final String name;
     private final String status;
     private final String created;
+    private final Map<String, String> labels;
     @SerializedName("public_net")
     private final PublicNet publicNet;
 
-    public Server(int id, String name, String status, String created, PublicNet publicNet) {
+    public Server(int id, String name, String status, String created, Map<String, String> labels, PublicNet publicNet) {
         this.id = id;
         this.name = name;
         this.status = status;
         this.created = created;
+        this.labels = labels;
         this.publicNet = publicNet;
     }
 
@@ -32,6 +36,10 @@ public class Server {
 
     public String getCreated() {
         return created;
+    }
+
+    public Map<String, String> getLabels() {
+        return labels;
     }
 
     public PublicNet getPublicNet() {
